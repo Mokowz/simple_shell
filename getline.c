ca56235599644767bf9a7aec3035e4ad97495330
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-12-05 08:29:54 -0800

    Add string functions

--- getline.c
+++ getline.c
@@ -12,7 +12,7 @@ ssize_t buffer_input(info_t *info, char **buffer, size_t *length)
 {
 	ssize_t bytesRead = 0;
 	size_t currentLength = 0;
-	
+
 	if (!*length) /* if nothing left in the buffer, fill it */
 	{
 		free(*buffer);
@@ -55,7 +55,7 @@ ssize_t get_input_line(info_t *info)
 	static size_t bufferIndex, j, bufferLength;
 	ssize_t bytesRead = 0;
 	char **currentBuffer = &(info->argument), *currentPosition;
-	
+
 	_putchar(BUF_FLUSH);
 	bytesRead = buffer_input(info, &commandBuffer, &bufferLength);
 	if (bytesRead == -1) /* EOF */
@@ -65,27 +65,25 @@ ssize_t get_input_line(info_t *info)
 		j = bufferIndex; /* initialize new iterator to current buffer position */
 		currentPosition = commandBuffer + bufferIndex; /* get pointer for return */
 		check_chain(info, commandBuffer, &j, bufferIndex, bufferLength);
-		
+
 		while (j < bufferLength) /* iterate to semicolon or end */
 		{
 			if (is_chain(info, commandBuffer, &j))
 				break;
 			j++;
 		}
-		
+
 		bufferIndex = j + 1; /* increment past nulled ';'' */
 		if (bufferIndex >= bufferLength) /* reached end of buffer? */
 		{
-            bufferIndex = bufferLength = 0; /* reset position and length */
-            info->cmd_buffer_type = CMD_NORMAL;
-        }
-
-        *currentBuffer = currentPosition; /* pass back pointer to current command position */
-        return _strlen(currentPosition); /* return length of current command */
-    }
-
-    *currentBuffer = commandBuffer; /* else not a chain, pass back buffer from _getline() */
-    return bytesRead; /* return length of buffer from _getline() */
+			bufferIndex = bufferLength = 0; /* reset position and length */
+			info->cmd_buffer_type = CMD_NORMAL;
+		}
+		*currentBuffer = currentPosition;
+		return (_strlen(currentPosition)); /* return length of current command */
+	}
+	*currentBuffer = commandBuffer;
+	return (bytesRead); /* return length of buffer from _getline() */
 }
 
 /**
@@ -98,17 +96,16 @@ ssize_t get_input_line(info_t *info)
  */
 ssize_t read_buffer(info_t *info, char *buffer, size_t *index)
 {
-    ssize_t bytesRead = 0;
+	ssize_t bytesRead = 0;
 
-    if (*index)
-        return 0;
+	if (*index)
+		return (0);
 
-    bytesRead = read(info->read_fd, buffer, READ_BUFFER_SIZE);
+	bytesRead = read(info->read_fd, buffer, READ_BUFFER_SIZE);
+	if (bytesRead >= 0)
+		*index = bytesRead;
 
-    if (bytesRead >= 0)
-        *index = bytesRead;
-
-    return bytesRead;
+	return (bytesRead);
 }
 
 /**
@@ -121,32 +118,42 @@ ssize_t read_buffer(info_t *info, char *buffer, size_t *index)
  */
 int _getline(info_t *info, char **pointer, size_t *length)
 {
-    static char buffer[READ_BUFFER_SIZE];
-    static size_t bufferIndex, bufferLength;
-    size_t k;
-    ssize_t bytesRead = 0, size = 0;
-    char *currentPointer = NULL, *newPointer = NULL, *c;
+	static char buffer[READ_BUFFER_SIZE];
+	static size_t bufferIndex, bufferLength;
+	size_t k;
+	ssize_t bytesRead = 0, size = 0;
+	char *currentPointer = NULL, *newPointer = NULL, *c;
 
-    currentPointer = *pointer;
+	currentPointer = *pointer;
+	if (currentPointer && length)
+		size = *length;
 
-    if (currentPointer && length)
-        size = *length;
+	if (bufferIndex == bufferLength)
+	{
+		bufferIndex = bufferLength = 0;
+	}
 
-    if (bufferIndex == bufferLength)
-    {
-        bufferIndex = bufferLength = 0;
-    }
+	bytesRead = read_buffer(info, buffer, &bufferLength);
+	if (bytesRead == -1 || (bytesRead == 0 && bufferLength == 0))
+		return (-1);
+	c = _strchr(buffer + bufferIndex, '\n');
+	k = c ? 1 + (unsigned int)(c - buffer) : bufferLength;
 
-    bytesRead = read_buffer(info, buffer, &bufferLength);
+	newPointer = _realloc(currentPointer, size, size ? size + k : k + 1);
+	if (!newPointer) /* MALLOC FAILURE! */
+		return (currentPointer ? free(currentPointer), -1 : -1);
 
-    if (bytesRead == -1 || (bytesRead == 0 && bufferLength == 0))
-        return -1;
+	if (size)
+		_strncat(newPointer, buffer + i, k - i);
+	else
+		_strncpy(newPointer, buffer + i, k - i + 1);
 
-    c = _strchr(buffer + bufferIndex, '\n');
-    k = c ? 1 + (unsigned int)(c - buffer) : bufferLength;
+	size += k - i;
+	i = k;
+	currentPointer = newPointer;
 
-    newPointer = _realloc(currentPointer, size, size ? size + k : k + 1);
-
-    if (!newPointer) /* MALLOC FAILURE! */
-        return (currentPointer ? free(currentPointer),
+	if (bufferLength)
+		*bufferLength = size;
+	*pointer = currentPointer;
+	return (size);
 }
--- shell.h
+++ shell.h
@@ -12,6 +12,60 @@
 #include <fcntl.h>
 #include <errno.h>
 
+
+/**
+ * struct lists - singly linked list
+ * @num: Number
+ * @str: String
+ * @next: Next pointer
+ */
+typedef struct lists
+{
+	int num;
+	char *str;
+	struct lists *next;
+} list_t;
+
+/**
+ * struct pseudoargs - contains pseudo arguments to pass to functions
+ * @argument: String from getline
+ * @argv: Array of strings
+ * @path: Path of curent cmd
+ * @argc: Argument count
+ * @line_count: Error count
+ * @fname: Filename
+ * @env: Linked list
+ * @alias: Alias node
+ * @linecount_flag: if on count this line
+ * @status: return status of the last exec
+ * @err_num: umber of erorr
+ * @cmd_buffer: Address of pointer of cmd_buf
+ * @cmd_buffer_type: for || && ;
+ * @readfd: fd to read line from
+ */
+typedef struct pseudoargs
+{
+	char *argument;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int readfd;
+	char *fname;
+	list_t *env;
+	list_t *alias;
+	int status;
+	int linecount_flag;
+	int err_num;
+	char **cmd_buffer;
+	int cmd_buffer_type;
+} info_t;
+
+#define BUF_FLUSH -1
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL}
+
 ssize_t buffer_input(info_t *info, char **buffer, size_t *length);
 ssize_t get_input_line(info_t *info);
 ssize_t read_buffer(info_t *info, char *buffer, size_t *index);
@@ -31,5 +85,15 @@ int _atoi(char *s);
 
 /* strtok.c file */
 char **_strtok(char *str, char *delim);
+
+/* str_functs.c file */
+char *_strdup(const char *str);
+char *_strcpy(char *dest, char *src);
+void _puts(char *str);
+int _putchar(char c);
+int _strlen(char *str);
+int _strcmp(char *str1, char *str2);
+char *starts_with(const char *str, const char *small);
+char *_strcat(char *dest, char *src);
 
 #endif
