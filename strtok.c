0275ef7ac84c8192f53b484e994ba9a5f5397dc2
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-12-05 05:25:29 -0800

    Move is_delim funct to strtok.c file

--- strtok.c
+++ strtok.c
@@ -1,6 +1,20 @@
 #include "shell.h"
 
 /**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
  * **_strtok - splits a string into words
  * @str: String
  * @delim: Delimeter
