35eb561ab6e74c840227e3b65d92d2a3ae20d7bf
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-12-14 12:11:17 -0800

    Fix moree errors

--- more.c
+++ more.c
@@ -8,7 +8,7 @@ void sigHandler(int num)
 {
 	if (num == SIGINT)
 	{
-		_puts("S ");
+		_puts("\n#cisfun$ ");
 	}
 }
 
@@ -38,5 +38,5 @@ void end_file(int len, char *buffer)
 void isterm(void)
 {
 	if (isatty(STDIN_FILENO))
-		_puts("$ ");
+		_puts("#cisfun$ ");
 }
